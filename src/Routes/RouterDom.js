cc4a62ccaa2e2ca921c84b50b67caf7a90711085
juano2123 <juan_man.perlaza@uao.edu.co>
2024-02-10 16:18:49 -0500

    creacion de rutas y carpetas para una buena organizacion del proyecto

---
+++ src/Routes/RouterDom.js
@@ -0,0 +1,15 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import React from "react";
+
+// import InfoAccount from "../pages/Account/InfoAccount";
+const RoterDom = () => {
+  return (
+    <>
+      <Routes>
+      <Route path="/" element={<sd />} /> 
+      </Routes>
+    </>
+  );
+};
+
+export default RoterDom;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-
+import { BrowserRouter } from "react-router-dom";
+import RouterDom from "/Routes/RouterDom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RouterDom />
+
+    </BrowserRouter>
   </React.StrictMode>
 );
 
