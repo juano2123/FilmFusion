b30ad2562f846a3412cf75df3c90840a22665864
juan manuel perlaza <juan_man.perlaza@uao.edu.co>
2024-02-21 17:57:48 -0500

    componente de bontenes de intercambio a objeto

--- src/shared/components/barAr/BarAr.jsx
+++ src/shared/components/barAr/BarAr.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./barAr.css";
-import { Switch,ConfigProvider,Space } from 'antd';
-
+// import { Switch,ConfigProvider,Space } from 'antd';
 
 const BarAr = () => {
+  const [state, setstate] = useState("fill");
 
-
-  
-
-    
   return (
-    <div className='Switch' >
-      
-       <ConfigProvider
-    theme={{
-      token: {
-        // Seed Token
-        colorPrimary:'rgba(57, 57, 57, 0.7)',
-        colorTextQuaternary:'rgba(57, 57, 57, 0.7)',colorTextLightSolid:'rgba(10, 132, 255, 1)',fontSize:'3',fontSizeSM:'12',fontFamily:'SF UI Display'
-      },
-    }}
-  >
-    <Space>
-      <Switch checkedChildren="AR" unCheckedChildren="Objeto" defaultChecked />
-    </Space>
-  </ConfigProvider>
+    <div className="Switch">
+      <div className="botones">
+        {state === "fill" ? (
+          <>
+            <button class="btn" className="AR">
+              ar
+            </button>
 
+            <button class="btn">objet</button>
+          </>
+        ) : (
+          <>
+            <button class="btn">ar</button>
+
+            <button className="objet" class="btn">
+              objet
+            </button>
+          </>
+        )}
+        {/* <button className={state === "fill" ? "opcione1" : "opcione2"}>
+          sadsa
+        </button> */}
+      </div>
+      {/* <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary:'rgba(57, 57, 57, 0.7)',
+          colorTextQuaternary:'rgba(57, 57, 57, 0.7)',colorTextLightSolid:'rgba(10, 132, 255, 1)',fontSize:'3',fontSizeSM:'12',fontFamily:'SF UI Display'
+        },
+      }}
+    >
+      <Space>
+        <Switch checkedChildren="AR" unCheckedChildren="Objeto" defaultChecked />
+      </Space>
+    </ConfigProvider> */}
     </div>
-  )
-}
+  );
+};
 export default BarAr;
