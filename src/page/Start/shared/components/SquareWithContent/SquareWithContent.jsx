51f3b2a3b530328da8c0737e54aae87467e5ec71
Tatiespi <tatiana.espinosa@uao.edu.co>
2024-03-14 14:38:38 -0500

    arreglo del aviso

--- src/page/Start/shared/components/SquareWithContent/SquareWithContent.jsx
+++ src/page/Start/shared/components/SquareWithContent/SquareWithContent.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './SquareWithContent.css'; // Asegúrate de tener este archivo CSS para estilos
 
-const SquareWithContent = ({ text, size, color, borderRadius }) => {
+const SquareWithContent = ({ text1,text2, size, color, borderRadius }) => {
   // Definir el color de fondo con opacidad usando rgba
   const containerStyle = {
     width:  size === 'large' ? '352px' : '286px',
     height:  size === 'large' ? '144px' : '80px',
     backgroundColor: color,
-    borderRadius: '5px'
+    borderRadius: '25px'
   }
   
 //   'rgba(19, 6, 35, 0.71)';
 
   return (
     <div className="square-container" style={containerStyle }>
-      <p className="square-text">{text}</p>
+      <p className="square-text1">{text1}</p>
+      <p className="square-text2">{text2}</p>
     </div>
   );
 };
