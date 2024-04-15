4a2ed8b77622cfb0ee1c234647a379f166bc8972
Tatiespi <tatiana.espinosa@uao.edu.co>
2024-04-15 15:31:18 -0500

    se cambio las rutas para que quede mas organizado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-import RouterDom from "./Routes/RouterDom";
+import RouterDom from "../src/web-app/routes/RouterDom.js";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
