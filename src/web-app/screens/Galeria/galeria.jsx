db93421ee8272667a188ef7f81d9f10581c35d3f
juano2123 <juan_man.perlaza@uao.edu.co>
2024-04-18 01:47:46 -0500

    cambios en los flujos

--- src/web-app/screens/Galeria/galeria.jsx
+++ src/web-app/screens/Galeria/galeria.jsx
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { useSwipeable } from 'react-swipeable';
-import './galeria.css'; // Asegúrate de tener este archivo CSS
-import obtenerImagenUrl from '../../firebase/config'; // Ajusta la ruta
-import LeftArrow from './assets/left.svg';
-import RightArrow from './assets/right.svg';
+import React, { useState, useEffect } from "react";
+import { useSwipeable } from "react-swipeable";
+import "./galeria.css"; // Asegúrate de tener este archivo CSS
+import obtenerImagenUrl from "../../firebase/config"; // Ajusta la ruta
+import LeftArrow from "./assets/left.svg";
+import RightArrow from "./assets/right.svg";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import butonback from "./assets/LeftCircleOutlined.svg";
+import { useNavigate } from "react-router-dom";
+import filmy from "./assets/SelcciónArchivosCompleta.gif"
 
 const GaleriaPage = () => {
   const [indiceActivo, setIndiceActivo] = useState(0);
   const [imagenes, setImagenes] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function cargarImagenes() {
       try {
         const urls = await Promise.all([
-          obtenerImagenUrl('pentax/Foto 1 - Cataras del Niagara.png'),
-          obtenerImagenUrl('pentax/Foto 2 - Guitarra.png'),
-          obtenerImagenUrl('pentax/Foto 3 - Girasoles.png')
+          obtenerImagenUrl("pentax/Foto 1 - Cataras del Niagara.png"),
+          obtenerImagenUrl("pentax/Foto 2 - Guitarra.png"),
+          obtenerImagenUrl("pentax/Foto 3 - Girasoles.png"),
         ]);
-        setImagenes(urls.map((url, index) => ({
-          src: url,
-          descripcion: `Descripción ${index + 1}` // Puedes personalizar la descripción aquí
-        })));
+        setImagenes(
+          urls.map((url, index) => ({
+            src: url,
+            descripcion: `Descripción ${index + 1}`, // Puedes personalizar la descripción aquí
+          }))
+        );
       } catch (error) {
         console.error("Error al cargar imágenes:", error);
       }
@@ -29,32 +36,57 @@ const GaleriaPage = () => {
   }, []);
 
   const handlers = useSwipeable({
-    onSwipedLeft: () => cambiarImagen('siguiente'),
-    onSwipedRight: () => cambiarImagen('anterior'),
+    onSwipedLeft: () => cambiarImagen("siguiente"),
+    onSwipedRight: () => cambiarImagen("anterior"),
   });
+  
+const handleButtonClick = () => {
+  navigate("/objeto");
+};
 
   const cambiarImagen = (direccion) => {
-    if (direccion === 'siguiente') {
+    if (direccion === "siguiente") {
       setIndiceActivo((prevIndex) => (prevIndex + 1) % imagenes.length);
     } else {
-      setIndiceActivo((prevIndex) => (prevIndex - 1 + imagenes.length) % imagenes.length);
+      setIndiceActivo(
+        (prevIndex) => (prevIndex - 1 + imagenes.length) % imagenes.length
+      );
     }
   };
 
   if (!imagenes.length) return <div>Cargando imágenes...</div>;
 
   return (
-    <div className="galeria-container" {...handlers}>
-      <button onClick={() => cambiarImagen('anterior')} aria-label="Anterior" className="navigation-button">
-        <img src={LeftArrow} alt="Anterior" />
-      </button>
-      <div className="image-and-description">
-        <img src={imagenes[indiceActivo].src} alt={`Imagen ${indiceActivo + 1}`} />
-        <p className="descripcion">{imagenes[indiceActivo].descripcion}</p>
+    <div className="galeria-container">
+      <div className="button-exit">
+        <button>
+        <img src= {butonback} onClick={handleButtonClick}/> 
+        </button>
       </div>
-      <button onClick={() => cambiarImagen('siguiente')} aria-label="Siguiente" className="navigation-button">
-        <img src={RightArrow} alt="Siguiente" />
-      </button>
+      <div {...handlers} className="imagen-container">
+        <button onClick={() => cambiarImagen("anterior")} aria-label="Anterior">
+          <img src={LeftArrow} alt="Anterior" />
+        </button>
+        <div className="image-and-description">
+          <img
+            src={imagenes[indiceActivo].src}
+            alt={`Imagen ${indiceActivo + 1}`}
+          />{" "}
+          <div className="descripcion">
+            {imagenes[indiceActivo].descripcion}
+          </div>
+        </div>
+        <button
+          onClick={() => cambiarImagen("siguiente")}
+          aria-label="Siguiente"
+        >
+          <img src={RightArrow} alt="Siguiente" />
+        </button>
+      </div>
+      <div className='filmy-galery'>
+        <img src={filmy} alt="" />
+
+          </div>
     </div>
   );
 };
--- src/web-app/screens/Object/objeto.jsx
+++ src/web-app/screens/Object/objeto.jsx
@@ -3,6 +3,7 @@ import "./objeto.css"; // Asegúrate de que el archivo de estilos existe
 import Ar from "../../components/AR/Ar.jsx";
 import BarAr from "../../components/BarAr/BarAr.jsx";
 import AudioControls from "../../components/AudioControls/AudioControls";
+import Showgalery from "../../components/ShowGalery/Showgalery.jsx";
 
 const ObjetoPage = () => {
   return (
@@ -10,7 +11,10 @@ const ObjetoPage = () => {
       <div className="objet-container ">
         <BarAr></BarAr>
       </div>
-      <div className="">
+      <div className="galeria">
+          {/* <Showgalery/> */}
+      </div>
+      <div className="obj-3D">
         <Ar></Ar>
       </div>
       <div className="audio_obj">
