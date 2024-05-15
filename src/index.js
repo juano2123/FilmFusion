406f71c1cdb6ab1a875cbf42fe1b133edc2dc21b
Tatiespi <tatiana.espinosa@uao.edu.co>
2024-05-15 11:40:12 -0500

    realizar la pantalla emergente

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-import RouterDom from "../src/web-app/routes/RouterDom.js";
+import RouterDom from "../src/web-app/Routes/RouterDom.js";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
